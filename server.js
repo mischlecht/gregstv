81a8260e4c41ca188436ad53cf21ce4821342df6
Michael <mischlecht@gmail.com>
2014-02-19 11:45:21 -0600

    upload ability added to system

--- server.js
+++ server.js
@@ -9,12 +9,20 @@ var url = require("url");
 
 function start(route, handle) {
 	function onRequest (request, response) {
+		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
 
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		
-		route(handle, pathname, response);
+		request.setEncoding("utf8");
+
+		request.addListener("data", function (postDataChunk) {
+			postData += postDataChunk;
+			console.log("Received POST data chunk '" + postDataChunk + "'.");
+		});
+
+		request.addListener("end", function() {
+			route(handle, pathname, response, request);
+		});
 	}
 
 http.createServer(onRequest).listen(8888);
